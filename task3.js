18de60f114926094caa111d82b8fe641177d4b80
Rabin <lrabin935@gmail.com>
2024-12-24 20:12:08 +0545

    task to do

---
+++ task3.js
@@ -0,0 +1,19 @@
+// 1. Write a program using for loop to print Numbers from 1 to 10.
+
+// for (let i = 0; i <= 10; i++){
+//     console.log(i)
+// }
+
+// 2. Write a program using while loop to sum of Numbers from 1 to 100.
+
+// let j = 0;
+// while (j <= 100){
+//     console.log(j);
+//     j++;
+// }
+
+// 3. Write a program using for loop to print all even numbers between 1 and 20.
+
+for (let even = 0; even <=20; even++){
+    console.log(even);
+}
